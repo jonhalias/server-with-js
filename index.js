a8da29ae4dad933edd39341eb013c3c63abb1e1f
Julian G <juliangodoy260@gmail.com>
2024-10-21 12:46:10 -0300

    Cambio/ una direccion about

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const fs = require("fs");
 const app = http.createServer((req, res) => {
     if(req.url == "/"){
         const texto = fs.readFileSync("./index.html", "utf-8");
-        res.write(texto)
-        //fs.writeFileSync("./texto.txt", "Hola mundo");
-        //console.log(texto)
+        res.write(texto);
+    }
+    if(req.url == "/about"){
+        const texto = fs.readFileSync("./about.html", "utf-8");
+        res.write(texto);
     }
     res.end()
 })
 
 app.listen(5000)
 console.log("Server on http://localhost:5000");
-
-
