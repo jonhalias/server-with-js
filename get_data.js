c358bfeaa79c22ca15ab127a7a4af0b8c48ea6a6
julian <113127353+jonhalias@users.noreply.github.com>
2025-04-02 18:30:25 -0300

    Update get_data.js

--- get_data.js
+++ get_data.js
@@ -1,5 +1,5 @@
 const getData = async() => {
-  const res = await fetch("https://8080-idx-my-app-1732331556927.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev/api");
+  const res = await fetch("https://8500-idx-my-app-1732331556927.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev/api");
   const data = await res.json();
   console.log(data);
 }
