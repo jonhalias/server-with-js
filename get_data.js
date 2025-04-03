87ba484778a1a29b3c45ad8df7485a798beb85f7
julian <113127353+jonhalias@users.noreply.github.com>
2025-04-03 19:41:05 -0300

    Update get_data.js

--- get_data.js
+++ get_data.js
@@ -1,4 +1,4 @@
-fetch("https://8600-idx-my-app-1732331556927.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev/api")
+fetch("https://8700-idx-my-app-1732331556927.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev/api")
 .then(res => res.json())
 .then(data => {
   console.log(data)
